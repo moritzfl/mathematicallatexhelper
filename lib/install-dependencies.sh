75ee650c87c97194fe7aa7bee5796f655cf8279f
moritzfl <moritzf@gmail.com>
2019-06-11 18:37:26 +0200

    update build script

--- lib/install-dependencies.sh
+++ lib/install-dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
 mvn install:install-file -Dfile=jai_core-1.1.3.jar -DgroupId=Javax.media -DartifactId=jai_core -Dversion=1.1.3 -Dpackaging=jar
 mvn install:install-file -Dfile=mathocr-0.0.3.jar -DgroupId=com.github.chungkwong -DartifactId=mathocr -Dversion=0.0.3 -Dpackaging=jar 
